df7a41c24a87af58d5001fec84d93c7480157cc5
yiuflin <yiuflin@gmail.com>
2013-02-11 18:49:25 +0800

    force usb fast charge [faux123]
    Conflicts:
    	arch/arm/mach-msm/Kconfig

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -43,6 +43,11 @@
 #include <mach/msm_xo.h>
 #include <mach/msm_bus.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#define USB_FASTCHG_LOAD 1000 /* uA */
+#endif
+
 #define MSM_USB_BASE	(motg->regs)
 #define DRIVER_NAME	"msm_otg"
 
@@ -889,7 +894,14 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 
 	if (motg->cur_power == mA)
 		return;
-
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (force_fast_charge == 1) {
+			mA = USB_FASTCHG_LOAD;
+			pr_info("USB fast charging is ON - 1000mA.\n");
+	} else {
+		pr_info("USB fast charging is OFF.\n");
+	}
+#endif
 	dev_info(motg->otg.dev, "Avail curr from USB = %u\n", mA);
 	pm8921_charger_vbus_draw(mA);
 	motg->cur_power = mA;
